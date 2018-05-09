1fce08bbfbf40dcc7f1d66919b325a3bd86bcd71
Alireza <alireza6677@gmail.com>
2018-05-09 07:17:26 +0430

    new link replaced

--- background.js
+++ background.js
@@ -2,9 +2,7 @@ chrome.tabs.onCreated.addListener(
     function(tab) {
         if(tab.url.indexOf('30nama') > -1){
            console.log("30nama here");
-           newUrl = tab.url.replace(/30nama.*?\// , '30nama.us/');
+           newUrl = tab.url.replace(/30nama.*?\// , '30nama.io/');
            chrome.tabs.update(tab.id, {url: newUrl});
         }
-});
-
-
+    });
